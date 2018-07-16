041a485066b40a2de1051f82ddbc8af1592c3ea0
Mokhtari <mohammadmehdi.mokhtari@monster.com>
2018-07-16 09:54:02 +0800

    Initial commit

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Something", function(){
+    describe("This way it smells", function(){
+        it("is punget", function(){
+            assert.ok(true);
+        });
+    });
+});
